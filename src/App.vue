7addf7536e2309b5a0c92168a75e68f3ca865e94
Liam Sho <conan_creeper@outlook.com>
2023-06-10 02:10:16 +0800

    chore: repo init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { RouterView } from 'vue-router'
+import { themeChange } from 'theme-change'
+
+onMounted(() => {
+  themeChange(false)
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark', 'valentine', 'dracula'],
+    darkTheme: 'dark',
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: '',
+    logs: true
+  }
+}
