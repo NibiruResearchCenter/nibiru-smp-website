a008dc61b2ce2d23b0c12f3cf2bd4335f19ab302
Liam Sho <conan_creeper@outlook.com>
2023-10-10 08:33:05 +0800

    fix: move data fetch to onMount in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,24 @@ interface SightFile {
   directus_files_id: string;
 }
 
-const sights = await getItems<SightFile>({
-  collection: "nibiru_smp_sights_files",
-  params: {
-    fields: ["directus_files_id"],
-  },
+const fetchSights = async () => {
+  const sights = await getItems<SightFile>({
+    collection: "nibiru_smp_sights_files",
+    params: {
+      fields: ["directus_files_id"],
+    },
+  });
+
+  return sights.map((s) => s.directus_files_id);
+};
+
+const images = ref<string[]>([]);
+const image = ref<string>("");
+
+onMounted(async () => {
+  images.value = await fetchSights();
+  image.value = images.value[Math.floor(Math.random() * images.value.length)];
 });
-
-const images = sights.map((s) => s.directus_files_id);
-
-const image = ref(images[Math.floor(Math.random() * images.length)]);
 </script>
 
 <template>
