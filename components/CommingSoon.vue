14c787795f3112c1ac66bd957f3e7df20775d2be
Liam Sho <conan_creeper@outlook.com>
2023-10-13 17:40:58 +0800

    feat: add comming soon info for future tabs

---
+++ components/CommingSoon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hero min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold text-white">{{ $t("comming_soon") }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,11 +1,7 @@
 <template>
   <main>
-    <div class="min-h-screen">
-      <div class="pt-16" />
-      <div class="px-12 py-16 md:px-20 lg:px-40 xl:px-60 2xl:px-72">
-        <h1>Posts</h1>
-        <p>Coming soon...</p>
-      </div>
-    </div>
+    <ContentMainArea :with-padding="false">
+      <CommingSoon />
+    </ContentMainArea>
   </main>
 </template>
